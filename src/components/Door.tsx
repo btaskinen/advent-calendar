bb67aa772f06598daed5479a1721dcf0b73e943c
Barbara <barbara.taskinen@gmail.com>
2023-12-01 14:47:23 +0200

    Add date restriction
    - door can only be opened on the day of its date or after

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -9,20 +9,34 @@ type Props = {
 };
 
 const Door = ({ video, setModalOpen, setSelectedDoor }: Props) => {
-  const [doorOpen, setDoorOpen] = useState<boolean>(false);
   const { artist, doorNumber } = video;
 
+  const [doorOpen, setDoorOpen] = useState<boolean>(false);
+  const [doorText, setDoorText] = useState<string>(doorNumber);
+
+  const currentDate = new Date();
+
+  const doesOpen =
+    currentDate.getMonth() === 11 &&
+    currentDate.getDate() >= Number(doorNumber);
+
   const openDoor = () => {
-    setDoorOpen(true);
-    setModalOpen(true);
-    setSelectedDoor(video);
+    if (doesOpen) {
+      setDoorText(artist);
+      setDoorOpen(true);
+      setModalOpen(true);
+      setSelectedDoor(video);
+    } else {
+      setDoorOpen(true);
+      setDoorText('Not yet. Be patient!');
+    }
   };
 
   const doorStateClass = doorOpen ? 'open' : 'closed';
 
   return (
     <div className={`Door ${doorStateClass}`} onClick={openDoor}>
-      <div className="Door_innerDiv">{doorOpen ? artist : doorNumber}</div>
+      <div className="Door_innerDiv">{doorText}</div>
     </div>
   );
 };
